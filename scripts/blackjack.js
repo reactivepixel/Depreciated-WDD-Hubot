26b8e4580dc1a51f7ab8acf92c06e72907d13527
bogoroh <mtaatgen@live.com>
2014-06-22 22:51:58 -0400

    Added 4 random cards to blackjack

--- scripts/blackjack.js
+++ scripts/blackjack.js
@@ -21,14 +21,31 @@ var suits = new Array("H", "C", "S", "D");
 var cards = new Array();
 // changed 3 to 4 to display all four suits
 var cnt = 0;
-for(i=0; i<4; i++)
-    for(j=1; j<=13; j++)
+for(i=0; i<4; i++){
+    for(j=1; j<=13; j++){
         cards[cnt++] = suits[i] + j;
-
-
-for(x=0;x<cards.length;x++){
-	msg.send(cards[x]);
+    }
 }
+var random1 = Math.floor(Math.random()*cards.length)
+var card1 = cards[random1];
+cards.splice(random1,1);
+
+var random2 = Math.floor(Math.random()*cards.length)
+var card2 = cards[random2];
+cards.splice(random2,1);
+
+var random3 = Math.floor(Math.random()*cards.length)
+var card3 = cards[random3];
+cards.splice(random3,1);
+
+var random4 = Math.floor(Math.random()*cards.length)
+var card4 = cards[random4];
+cards.splice(random4,1);
+
+msg.send(card1);
+msg.send(card2);
+msg.send(card3);
+msg.send(card4);
 
 
 }
