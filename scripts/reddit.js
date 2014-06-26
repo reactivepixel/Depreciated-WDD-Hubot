8c43e488076bfe9a4d9b42b7cd08f68416900f29
bogoroh <mtaatgen@live.com>
2014-06-26 19:30:04 -0400

    Fixed syncronization

--- scripts/reddit.js
+++ scripts/reddit.js
@@ -17,23 +17,33 @@ var request = require('request')
 
 // Function to find the area of an areacode
 function reddit(msg){
-		request('http://www.reddit.com/r/random/top.json', function (error, response, body) {
-				// Make sure the connection went successfully through
-				if (!error && response.statusCode < 300){
-					// Parse the json
-					var json = JSON.parse(body);
-					var sub = json.data.children[0].data.subreddit;
-					var title = json.data.children[0].data.title;
-					var link =  json.data.children[0].data.permalink;
-					// Check to make sure the area code has been found
-					msg.send(title + " is the top title for " + "/r/"+ sub + ".")
-					msg.send("The link for this article is: " + "http://www.reddit.com" +link)
-				}
-				else{
-					//Return error if api request goes wrong.
-					msg.send("Something went wrong here.."); 
-				}	
-		});
+	// Array to output msg.send
+	var messageArray = [];
+	request('http://www.reddit.com/r/random/top.json', function (error, response, body) {
+		// Make sure the connection went successfully through
+		if (!error && response.statusCode < 300){
+			// Parse the json
+			var json = JSON.parse(body);
+			var sub = json.data.children[0].data.subreddit;
+			var title = json.data.children[0].data.title;
+			var link =  json.data.children[0].data.permalink;
+			// Check to make sure the area code has been found
+			messageArray.push(title + " is the top title for " + "/r/"+ sub + ".")
+			messageArray.push("The link for this article is: " + "http://www.reddit.com" +link)
+
+			// Output the strings back
+			for(z = 0; z <= messageArray.length; z++){
+		    (function(z){
+		        setTimeout(function(){
+		            msg.send(messageArray[z])
+		        }, 500 * z);
+		    }(z));
+		}
+		else{
+			//Return error if api request goes wrong.
+			msg.send("Something went wrong here.."); 
+		}	
+	});
 }
 
 //Listens for the exact match of "blackjack" and calls playJack function.
