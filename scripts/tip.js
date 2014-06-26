a927743a2ba8a8eb3a90b2ec646a14fab6c944eb
Jairo <JJEcu13@hotmail.com>
2014-06-26 18:40:10 -0400

    changed messages to show amount entered and update package.json

--- scripts/tip.js
+++ scripts/tip.js
@@ -13,20 +13,20 @@
 // Author:
 // Jairo Jurado
 
-// Dependency for dollar amounts with commas.
+// Dependency for dollar amounts with dollar sign and commas.
 var numeral = require('numeral');
 
 // Function to calculate the tip amount.
 function getTip(msg) {
 	// Variable to hold user's input.
-	var amount = msg.match[1].trim();
+	var amount = msg.match[1].trim().replace(',', '.');
 	// Converts amount variable to a decimal and holds it in a new variable.
 	var amountDecimal = parseFloat(amount);
 	
 	// Checks whether the amountDecimal variable is a number or not.
 	if(isNaN(amountDecimal)) {
 		// If the user's input is not a number, the user is informed to only enter numbers.
-		msg.send("There are letters and/or punctuation marks in your input. Please only enter numbers.");
+		msg.send("Please only enter numbers.");
 	} else { // If the user's input is a number, calculations will run.
 		
 		// Calculates the tips.
@@ -34,12 +34,13 @@ function getTip(msg) {
 		var tip15 = .15 * amountDecimal;
 		var tip20 = .20 * amountDecimal;
 		
-		// Adds tip with original amount to get total.
+		// Adds tip with original amounts to get totals.
 		var total10 = tip10 + amountDecimal;
 		var total15 = tip15 + amountDecimal;
 		var total20 = tip20 + amountDecimal;
 		
-		// Sends the calculated tip and total to the user.
+		// Sends the calculated tip and total to the user. Formats numerical values to dollar values.
+		msg.send("Amount entered: "+ numeral(amountDecimal).format('$0,0.00') +".");
 		msg.send("10% tip is "+ numeral(tip10).format('$0,0.00') +". Total is "+ numeral(total10).format('$0,0.00') +".");
 		msg.send("15% tip is "+ numeral(tip15).format('$0,0.00') +". Total is "+ numeral(total15).format('$0,0.00') +".");
 		msg.send("20% tip is "+ numeral(tip20).format('$0,0.00') +". Total is "+ numeral(total20).format('$0,0.00') +".");
