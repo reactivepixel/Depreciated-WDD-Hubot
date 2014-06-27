9d97786ee6d8a0838e84dd6d3b28746af75acf2e
bogoroh <mtaatgen@live.com>
2014-06-26 20:07:08 -0400

    changed format for what is returned; added comments, fixed spelling errors - Jairo

--- scripts/randReddit.js
+++ scripts/randReddit.js
@@ -1,5 +1,5 @@
 // Description:
-//   Hubot gives you a random subreddit with it's top title for the day
+//   Hubot gives you a random subreddit with its top title for the day
 //
 // Dependencies:
 //   Request
@@ -8,29 +8,32 @@
 //   None
 //
 // Commands:
-//   Hubot redtop  - Gets you a random subreddit with the top post
+//   Hubot redtop  - Gets you a random subreddit with its top post
 //
 // Author:
 //   Bogoroh
 
 var request = require('request')
 
-// Function to find the area of an areacode
+// Function to find a random top subreddit.
 function reddit(msg){
-	// Array to output msg.send
 	
 	request('http://www.reddit.com/r/random/top.json', function (error, response, body) {
-		// Make sure the connection went successfully through
+		// Conditional to make sure the connection went successfully through
 		if (!error && response.statusCode < 300){
+			// Array to output msg.send
 			var messageArray = [];
-			// Parse the json
+			// Parses the json
 			var json = JSON.parse(body);
 			var sub = json.data.children[0].data.subreddit;
 			var title = json.data.children[0].data.title;
 			var link =  json.data.children[0].data.permalink;
-			// Check to make sure the area code has been found
-			messageArray.push(title + " for " + "/r/"+ sub + ".")
-			messageArray.push("Article: " + "http://www.reddit.com" +link)
+
+			// Pushes to messageArray
+			//messageArray.push(title + " for " + "/r/"+ sub + ".");
+			messageArray.push("Article: "+ title +".");
+			messageArray.push("Subreddit: "+ sub + ".");
+			messageArray.push("Link: " + "http://www.reddit.com" +link);
 
 			// Output the strings back
 			for(z = 0; z <= messageArray.length; z++){
@@ -42,13 +45,13 @@ function reddit(msg){
 			}
 		}
 		else{
-			//Return error if api request goes wrong.
-			msg.send("Something went wrong here.."); 
+			//Returns error if api request goes wrong.
+			msg.send("Something went wrong here."); 
 		}	
 	});
 }
 
-//Listens for the exact match of "blackjack" and calls playJack function.
+//Listens for the exact match of "redtop" and calls reddit function.
 module.exports = function(robot) {
 	return robot.respond(/redtop/i, function(msg) {
 		reddit(msg);
