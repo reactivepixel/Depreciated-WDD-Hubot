144054e7d43fd49318e5f6bdbf649cfe7911cd0e
bkerickson <bkerickson@fullsail.edu>
2014-08-25 20:36:20 -0400

    clarify comments, slack formatting added

--- scripts/googl-shortener.js
+++ scripts/googl-shortener.js
@@ -23,18 +23,19 @@ function googlShortener(msg){
 
 	// define a regex pattern to test the entered url
 	var urlRegexPattern = new RegExp('(http(s)?:\/\/)?([a-zA-Z\-]*\.)+(com|org|net|edu|gov)(\/.*(\/)*)*');
+
 	// test if a url was entered and that it passes the regex test
+	// else no url was entered or it was not valid, set the variable to an empty string
 	if(msg.match[1] && urlRegexPattern.test(msg.match[1])){
-		// save the entered url and trim it of whitespace
+		// save the entered url and trim it of any whitespace
 		googlEnteredUrl = msg.match[1];
 		googlEnteredUrl = googlEnteredUrl.trim();
 	}else{
-		// if no url was entered or it was not valid, set the variable to an empty string
 		googlEnteredUrl = "";
 	}
 	// if the string length is 0 as a result of a failed regex or no entry, send an error message
 	if(googlEnteredUrl.length == 0){
-		msg.send("Please check the URL you entered or enter a URL to be shortened, and try again. IE: hubot googl www.google.com");
+		msg.send("Please check the URL you entered or enter a URL to be shortened, and try again. Note: only .com, .org, .net, .edu, and .gov can be used. IE: hubot `googl www.google.com`");
 		return;
 	}
 
@@ -50,12 +51,12 @@ function googlShortener(msg){
 	// send the request to the goo.gl API
 	request(googlPostOptions, function (error, response, body) {
 		// if no errors came back and the status is good, display the new shortened URL
-		if (!error && response.statusCode == 200) {
+		if (!error && response.statusCode < 300) {
 			msg.send(response.body.longUrl + " was successfully shortened.")
-			msg.send("Your shortened URL is: " + response.body.id);
+			msg.send("Your shortened URL is: `" + response.body.id + "`");
 		}else{
 			// if there was an error with the API call, send an error message
-			msg.send("Google was not able to shorten your url. Please check that you entered it correctly.");
+			msg.send("Google could not shorten your url, or the URL shortener is down at the moment. Please check that you entered your URL correctly and try again in a few minutes.");
 		}
 
 	}); // end of request
