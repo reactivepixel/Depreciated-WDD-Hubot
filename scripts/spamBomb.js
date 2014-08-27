a5111c8da8741bcb5c06a168fb13764c0401795b
Austin Mayer <swipermail@icloud.com>
2014-08-27 15:00:45 -0400

    fixed variable names, reduced bomb, fixed catch

--- scripts/spamBomb.js
+++ scripts/spamBomb.js
@@ -8,15 +8,15 @@
 // N/A
 //
 // Commands:
-// Hubot textbomb <amount> <user>: <message> - Makes hubot spam a user next time they talk.
+// Hubot spambomb <amount> <user>: <message> - Makes hubot spam a user next time they talk.
 //
 // Author:
 // Austin Mayer
 // austinemayer@gmail.com
 
-var appendTextBomb;
+var appendSpamBomb;
 
-appendTextBomb = function(data, toUser, fromUser, message) {
+appendSpamBomb = function(data, toUser, fromUser, message) {
   var name;
   data[name = toUser.name] || (data[name] = []);
   return data[toUser.name].push([fromUser.name, message]);
@@ -26,21 +26,21 @@ module.exports = function(robot) {
   robot.brain.on('loaded', (function(e) {
     return function() {
       var base;
-      return (base = robot.brain.data).textBombs || (base.textBombs = {});
+      return (base = robot.brain.data).spamBombs || (base.spamBombs = {});
     };
   })(this));
-  robot.respond(/textbomb( \b([1-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0])\b) (.*?): (.*)/i, function(msg) {
-    if(msg.match[3].trim() == 0||msg.match[3].trim() > 50){
-      msg.send("Please enter a number between 1 and 50");
+  robot.respond(/spambomb (\b([1-9]|1[0-9]|2[0])\b) (.*?): (.*)/i, function(msg) {
+    if(msg.match[3].trim() === 0||msg.match[3].trim() > 20){
+      msg.send("Please enter a number between 1 and 20");
     }else{
       var user, users;
       users = robot.brain.usersForFuzzyName(msg.match[3].trim());
       if (users.length === 1) {
         user = users[0];
         for (i = 0, len = msg.match[2].trim(); i < len; i++) {
-          appendTextBomb(robot.brain.data.textBombs, user, msg.message.user, msg.match[4]);
+          appendSpamBomb(robot.brain.data.spamBombs, user, msg.message.user, msg.match[4]);
         }
-        return msg.send("textBomb prepared");
+        return msg.send("Spam bomb prepared");
       } else if (users.length > 1) {
         return msg.send("Too many users like that");
       } else {
@@ -49,16 +49,16 @@ module.exports = function(robot) {
     }
   });
   return robot.hear(/./i, function(msg) {
-    var textBomb, textBombs, i, len;
-    if (robot.brain.data.textBombs == null) {
+    var spamBomb, spamBombs, i, len;
+    if (robot.brain.data.spamBombs == null) {
       return;
     }
-    if ((textBombs = robot.brain.data.textBombs[msg.message.user.name])) {
-      for (i = 0, len = textBombs.length; i < len; i++) {
-        textBomb = textBombs[i];
-        msg.send(textBomb[1]);
+    if ((spamBombs = robot.brain.data.spamBombs[msg.message.user.name])) {
+      for (i = 0, len = spamBombs.length; i < len; i++) {
+        spamBomb = spamBombs[i];
+        msg.send(spamBomb[1]);
       }
-      return delete robot.brain.data.textBombs[msg.message.user.name];
+      return delete robot.brain.data.spamBombs[msg.message.user.name];
     }
   });
 };
