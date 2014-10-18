b79c4f3d3085bd10a36af77a603242850d4f8709
Eli <DGerena@EYE-OF-SAURON.local>
2014-10-18 19:43:01 -0400

    Fixed and finalizing working process. Still editing.

--- scripts/huClear.js
+++ scripts/huClear.js
@@ -8,16 +8,35 @@
 //   None
 //
 // Commands:
-//   Hubot clear - Clears the current height chat.
+//   Hubot clear - <clear desktop> clears the desktop avg line height.
+//					<clear mobile> clears the mobile avg line height.
+//					<clear tablet> clears the tablet avg line height.
 //
 // Author: dgernea
 
 
 function seeThrough(msg){
-	msg.send("\f");
+	for(var esc = 0;esc < msg.count; esc++){
+		msg.send(" ");
+	}
 }
 module.exports = function(robot) {
-	return robot.respond(/clear/i,function(msg){
-		seeThrough(msg);
+	return robot.respond(/clear\sdesktop|clear\smobile|clear\stablet/i,function(msg){
+		console.log("msg "+msg.message.text);
+		if(msg.message.text == "Hubot clear desktop"){
+			msg.count = 25;
+			seeThrough(msg);
+			console.log("25");
+		}else if(msg.message.text == "Hubot clear mobile"){
+			msg.count = 11;
+			seeThrough(msg);
+			console.log("10");
+		}else if(msg.message.text == "Hubot clear tablet"){
+			msg.count = 21;
+			seeThrough(msg);
+			console.log("20");
+		}else{
+			msg.send("Hey! I can't understand you when you mumble.")
+		}
 	});
 }
