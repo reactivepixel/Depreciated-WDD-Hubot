dc4f0dd0801c60ee7a1153e12ba219240681d0af
Holly Springsteen <hhspringsteen@gmail.com>
2014-10-18 19:25:00 -0400

    calc.js - calculator function that works with all simple expressions (PEMDAS, sin, cos, unit converter, etc.) / area.js - now converts state acronym to actual state name. Also displays timezone and current time for the requested areacode.

--- scripts/calc.js
+++ scripts/calc.js
@@ -14,13 +14,23 @@
 // Holly Springsteen
 // hhspringsteen@gmail.com
 
+// reuqire mathjs for calculation of expressions
 var math = require('mathjs');
 
-//Requests random fact from mentalfloss.com api
+// function to take expression of from user and convert it
 function calculator(msg){
-	var expression = msg.match[1],
-		regExpressions = math.eval(msg.match[1]);
-	msg.send(expression+" = "+regExpressions);
+	// lets you test a block of code for errors
+	try {
+		// expression from user
+		var expression = msg.match[1],
+			regExpressions = math.eval(msg.match[1]); // evaluate expression with mathjs
+		// send result
+		msg.send(expression+" = "+regExpressions);
+	}
+	// lets you handle the error
+	catch(err){
+		msg.send("Please enter a valid expression");
+	}
 }
 
 //Listens for the exact match of random fact and calls random fact function.
