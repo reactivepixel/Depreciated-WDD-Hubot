23bdb15e1d0a421ab87e27e30d83ab26e89450ef
eddiegemayel <eddiegem16@gmail.com>
2014-10-20 12:32:48 -0400

    my rea get weather file to be pushed

--- scripts/getWeather.js
+++ scripts/getWeather.js
@@ -1,5 +1,5 @@
 // Description:
-//   Enter a city in florida and get the respective current weather report
+//   Enter a valid zipcode to get the current weather report
 //
 // Dependencies:
 //   Request
@@ -8,26 +8,29 @@
 //   None
 //
 // Commands:
-//   Hubot show weather <city> - Shows current Weather for a City in Florida
+//   Hubot show weather <zipcode> - Shows current Weather for a location
 //
 // Author:
 //   Eddie Gemayel
 
-//Storing request in variable
 var request = require('request');
 
-// Function to find the weather of the city entered
+// Function to find the weather of the location entered
 function getWeather(msg){
-	
-	var city = msg.match[1]; // Get what the user entered
-	var	regex = /[^\w\s]/gi; // Regex for removing specials characters.
-	var	code = city.replace(regex, ""); // Remove regex characters.
-	
-	// If what the user entered is a string
-	if (isNaN(code)) {
 
-		// codeStr = code.toString(), // Stringify the "code" variable.
-		apiURL = 'http://api.wunderground.com/api/00bacbd3046f5248/conditions/q/FL/'+code+'.json';
+	//get zipcode the user entered
+	var zipcode = msg.match[1];
+	
+	// Conditional statement to check if user entered an integer or not
+	if (isNaN(zipcode)) {
+
+		//tell them they messed up if they entered a string
+		msg.send("The location you entered is invalid. Be sure you are entering a valid zipcode and try again.");
+
+	}else {
+		
+		//url for the API stored in a variable
+		apiURL = 'http://api.wunderground.com/api/00bacbd3046f5248/conditions/q/'+zipcode+'.json';
 		
 		// make the request to the api
 		request(apiURL, function (error, response, body) {
@@ -35,36 +38,33 @@ function getWeather(msg){
 				// This conditional makes sure the connection goes thru successfully
 				if (!error && response.statusCode < 300){
 
-					// Parse the json
+					// Parse the incoming json
 					var json = JSON.parse(response.body);
 
-
+					//if there is no errors in retrieving location
 					if(!json.response.error){
 
+						//spit back out the information to the user
 						msg.send("The city of " + json.current_observation.display_location.city + 
-						" in " + json.current_observation.display_location.state + 
+						" in " + json.current_observation.display_location.state_name + 
 						" is currently "+ json.current_observation.weather + 
 						" with " + json.current_observation.wind_mph + 
 						" mph winds. It's now "+ json.current_observation.temp_f + 
 						" degrees farenheit." );
-					// msg.send(json);
 
+					//if there is an error
 					}else{
+						//display that specific error message from the json
 						var error = json.response.error.description;
+						//spit back out the error to the user
 						msg.send(error);
 					}
-					
 				}
-				
 		});
-
-
-	}else {
-		msg.send("The city you entered is invalid. Be sure to check your spelling.");
 	}
-}
+}//end of function
 
-// Listens for the exact match of weather and calls getWeather function.
+// Listens for the exact match of show weather and calls the getWeather function.
 module.exports = function(robot) {
 	return robot.respond(/show weather (.*)/i, function(msg) {
 		getWeather(msg);
