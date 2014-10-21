9cb42a285105f836aaac9a63f5caf940be0cf7ce
Holly Springsteen <hhspringsteen@gmail.com>
2014-10-21 16:11:45 -0400

    fallback for excuses if developerexcuses.com is down

--- scripts/excuses.js
+++ scripts/excuses.js
@@ -3,6 +3,7 @@
 //
 // Dependencies:
 // cheerio
+// request
 //
 // Configuration:
 // none
@@ -15,19 +16,19 @@
 // hhspringsteen@gmail.com
 
 // cheerio scraper
-var cheerio = require('cheerio');
+var cheerio = require('cheerio'),
+	request = require('request');
 
 module.exports = function(robot) {
 	return robot.respond(/excuse me/i, function(msg) {
-		try{
-			return robot.http("http://developerexcuses.com/").get()(function(err, res, body) {
+		request("http://developerexcuses.com/", function (err, res, body) {
+			if (!err && res.statusCode < 300){
 				// get excuses from developerexcuses.com
 				var $ = cheerio.load(body); // scrape the site for the body
-				return msg.send($('.wrapper a').text()); // send back an excuse
-			});
-		}
-		catch(err){
-			msg.send("Sorry no excuses right now, developerexcuses.com is down :(");
-		}
+				msg.send($('.wrapper a').text()); // send back an excuse
+			}else{
+				msg.send("Sorry but developerexcuses.com is down");
+			}
+		});
 	});
 };
