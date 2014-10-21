747bcc75ed4672c73fc54877aff722fe6c98a4d6
Eli <dgerena87d@gmail.com>
2014-10-21 14:35:35 -0400

    Working on 2 of 3 hears and the response will use a switch statment pulling all from master push

--- scripts/huClear.js
+++ scripts/huClear.js
@@ -26,33 +26,26 @@ function seeThrough(msg){
 // regex can be awesome or a pain in the rear.
 module.exports = function(robot) {
 	return robot.respond(/clear\sdesktop$|clear\smobile$|clear\stablet$/i,function(msg){
-		console.log("msg "+msg.message.text);
 		//Filtering out the possible context to which someone might try to use the huclear.js/ Will work with the local and staging bots.
 		//Suggested fixes or upgrades are to extend this to work with anyones bot named anything. Or to function while somehow getting the viewing size.
 		if(msg.message.text == "Derpbot clear desktop"){
 			msg.count = 25;
 			seeThrough(msg);
-			console.log("25");
 		}else if(msg.message.text == "Derpbot clear mobile"){
 			msg.count = 11;
 			seeThrough(msg);
-			console.log("10");
 		}else if(msg.message.text == "Derpbot clear tablet"){
 			msg.count = 21;
 			seeThrough(msg);
-			console.log("20");
 		}else if(msg.message.text == "Hubot clear desktop"){
 			msg.count = 25;
 			seeThrough(msg);
-			console.log("25");
 		}else if(msg.message.text == "Hubot clear mobile"){
 			msg.count = 11;
 			seeThrough(msg);
-			console.log("10");
 		}else if(msg.message.text == "Hubot clear tablet"){
 			msg.count = 21;
 			seeThrough(msg);
-			console.log("20");
 		}else{
 			//God forbid you somehow do get past all the above text comparisons and didnt misscapitalize the bots Ill explain you need to learn to speak clearly.
 			msg.send("Hey! I can't understand you when you mumble make sure to capitalize my awesome name, all right.")
