fc2aa43d74aacd706275b605134630477d074e6b
Eli <dgerena87d@gmail.com>
2014-10-21 04:21:24 -0400

    Had to make a variation occur with responses. Feels better now.

--- scripts/huGratitude.js
+++ scripts/huGratitude.js
@@ -12,11 +12,14 @@
 //
 // Author: 
 //	 dgerena aka Eli!
-
+//varied responses for gratitude
+var responseArr=[
+	"Anytime user.","No problem ,just doing my thang."
+]
 //Have the robots accept gratitude from the users.
 module.exports = function(robot){
 	return robot.hear(/^thank\syou|Hubot$|thanks\shubot$|thanks\sderpbot$|thanks\sDerpbot$/i, function(msg){
   		//Heh giving the bots a bit of boasting
-    	msg.send("Anytime user."); 
+    	msg.send(responseArr[Math.floor(Math.random()*2)]); 
 	});
 }
