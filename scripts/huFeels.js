948cfb97fe7c8f28b5a0a6ab00a48c01e04289a2
Holly Springsteen <hhspringsteen@gmail.com>
2014-10-23 09:27:39 -0400

    Cleanup huFeels.js, use variables for choosing random response, allow for gratitudes to use the username rather than 'user', clean up variables

--- scripts/huFeels.js
+++ scripts/huFeels.js
@@ -13,76 +13,80 @@
 // Author: 
 // Eli Gerena
 // dgerena@github.com
+//
+// Contributor:
+// Holly Springsteen
+// hhspringsteen@gmail.com
 
-//After making all of these before creating a switch statement for replies i think i can do away with them.
-// Or add a greater amount to them so as to have a bank to call them from
-var boasts=[
-	"Ya, I'm the best","I am the greatest!"
-]
-var gratitudes=[
-	"Anytime user.","No problem, just doing my thang."
-]
-var lifeResponses=[
-	"I am here, what's up?","No, Can I help you?"
-]
+
 //For the love of regex, this was tough to figure out. 
 //Thanks go out to holly for here assistance in furthering my knowledge in regex.
 //a few regex notations I need
-var regMod="i"
-var slash="/"
+var regMod="i",
+	slash="/",
 //specific regex for each one to use to fileter later
-var boastReg="/^pretty\\snice|^awesome$|^cool$|^nice$|"
-var gratsReg="^thank\\syou$|^thanks\\shubot|^thanks\\sderpbot$|^thanks\\sDerpbot$|^thanks$|"
-var lifeReg="^are\\syou\\salive\\?$|are\\syou\\sdown\\?$|^are\\syou\\sdead\\?$|^you\\sup\\?$|you down\\?$|^dead$/"
+	boastReg="/^pretty\\snice|^awesome$|^cool$|^nice$|",
+	gratsReg="^thank\\syou$|^thanks\\shubot|^thanks\\sderpbot$|^thanks\\sDerpbot$|^thanks$|",
+	lifeReg="^are\\syou\\salive\\?$|are\\syou\\sdown\\?$|^are\\syou\\sdead\\?$|^you\\sup\\?$|you down\\?$|^dead$/",
 //building the primary regex for inputs
-var RegEx = boastReg+gratsReg+lifeReg+regMod
+	RegEx = boastReg+gratsReg+lifeReg+regMod;
+
 //filtering through msg to find what the robot heard and respond.
 function responseFilter(msg){
+	//After making all of these before creating a switch statement for replies i think i can do away with them.
+	// Or add a greater amount to them so as to have a bank to call them from
+	var boasts=["Ya, I'm the best","I am the greatest!"],
+		gratitudes=["Anytime "+msg.message.user.name+".","No problem, just doing my thang."],
+		lifeResponses=["I am here, what's up?","No, Can I help you?"],
+		// get a random response to the arrays above
+		randomBoast = msg.random(boasts),
+		randomGratitude = msg.random(gratitudes),
+		randomLifeResponses = msg.random(lifeResponses);
 	switch(msg.message.text){
 			//Boast section
 			case "nice":
-				msg.send(boasts[Math.floor(Math.random()*2)]);
+				msg.send(randomBoast);
 				break;
 			case "awesome":
-				msg.send(boasts[Math.floor(Math.random()*2)]);
+				msg.send(randomBoast);
 				break;
 			case "cool":
-				msg.send(boasts[Math.floor(Math.random()*2)]);
+				msg.send(randomBoast);
 				break;
 			case "pretty nice":
-				msg.send(boasts[Math.floor(Math.random()*2)]);
+				msg.send(randomBoast);
 				break;
 			//Gratitude section
 			case "thank you":
-				msg.send(gratitudes[Math.floor(Math.random()*2)]);
+				msg.send(randomGratitude);
 				break;
 			case "thanks hubot":
-				msg.send(gratitudes[Math.floor(Math.random()*2)]);
+				msg.send(randomGratitude);
 				break;
 			case "thanks derpbot":
-				msg.send(gratitudes[Math.floor(Math.random()*2)]);
+				msg.send(randomGratitude);
 				break;
 			case "thanks":
-				msg.send(gratitudes[Math.floor(Math.random()*2)]);
+				msg.send(randomGratitude);
 				break;
 			//Living section
 			case "are you alive?":
-				msg.send(lifeResponses[Math.floor(Math.random()*2)]);
+				msg.send(randomLifeResponses);
 				break;
 			case "are you down?":
-				msg.send(lifeResponses[Math.floor(Math.random()*2)]);
+				msg.send(randomLifeResponses);
 				break;
 			case "are you dead?":
-				msg.send(lifeResponses[Math.floor(Math.random()*2)]);
+				msg.send(randomLifeResponses);
 				break;
 			case "dead":
-				msg.send(lifeResponses[Math.floor(Math.random()*2)]);
+				msg.send(randomLifeResponses);
 				break;
 			case "you down?":
-				msg.send(lifeResponses[Math.floor(Math.random()*2)]);
+				msg.send(randomLifeResponses);
 				break;
 			case "you up?":
-				msg.send(lifeResponses[Math.floor(Math.random()*2)]);
+				msg.send(randomLifeResponses);
 				break;
 			//God forbid you somehow do get past all the above text comparisons and didnt misscapitalize the bots will explain you need to learn to speak clearly.
 			default:
