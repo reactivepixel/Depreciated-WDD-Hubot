a1e6eb8a8656e4fc3906fb0d970e2582efa7f94a
Eli <dgerena87d@gmail.com>
2014-10-23 00:34:23 -0400

    Fixed up clear worked out switch statement.

--- scripts/huClear.js
+++ scripts/huClear.js
@@ -1,5 +1,5 @@
 // Description:
-//   Clear the page by moveing the content upwards in the window.
+//   Clear the page by moving the content upwards in the window.
 //
 // Dependencies:
 //   None
@@ -8,9 +8,9 @@
 //   None
 //
 // Commands:
-//  Hubot clear desktop clears the desktop avg line height.
-//	Hubot clear mobile clears the mobile avg line height.
-//	Hubot clear tablet clears the tablet avg line height.
+//  (capitalize) Robot clear desktop clears the desktop avg line height.
+//	Robot clear mobile clears the mobile avg line height.
+//	Robot clear tablet clears the tablet avg line height.
 //
 // Author: 
 //	 dgerena aka Eli!
@@ -26,36 +26,34 @@ function seeThrough(msg){
 // regex can be awesome or a pain in the rear.
 module.exports = function(robot) {
 	return robot.respond(/clear\sdesktop$|clear\smobile$|clear\stablet$/i,function(msg){
-		console.log("msg "+msg.message.text);
 		//Filtering out the possible context to which someone might try to use the huclear.js/ Will work with the local and staging bots.
 		//Suggested fixes or upgrades are to extend this to work with anyones bot named anything. Or to function while somehow getting the viewing size.
-		if(msg.message.text == "Derpbot clear desktop"){
-			msg.count = 25;
-			seeThrough(msg);
-			console.log("25");
-		}else if(msg.message.text == "Derpbot clear mobile"){
-			msg.count = 11;
-			seeThrough(msg);
-			console.log("10");
-		}else if(msg.message.text == "Derpbot clear tablet"){
-			msg.count = 21;
-			seeThrough(msg);
-			console.log("20");
-		}else if(msg.message.text == "Hubot clear desktop"){
-			msg.count = 25;
-			seeThrough(msg);
-			console.log("25");
-		}else if(msg.message.text == "Hubot clear mobile"){
-			msg.count = 11;
-			seeThrough(msg);
-			console.log("10");
-		}else if(msg.message.text == "Hubot clear tablet"){
-			msg.count = 21;
-			seeThrough(msg);
-			console.log("20");
-		}else{
-			//God forbid you somehow do get past all the above text comparisons and didnt misscapitalize the bots Ill explain you need to learn to speak clearly.
-			msg.send("Hey! I can't understand you when you mumble make sure to capitalize my awesome name, all right.")
+		//Still couldnt somehow pass in the robot to concat onto the string clear desktop to make dynamic for each robot its plugged into.
+		switch(msg.message.text){
+			//desktop clear
+			case"Derpbot clear desktop":
+			case"Hubot clear desktop":
+				msg.count = 25;
+				seeThrough(msg);
+				console.log("26");
+				break;
+			//mobile clear
+			case"Derpbot clear mobile":
+			case"Hubot clear mobile":
+				msg.count = 11;
+				seeThrough(msg);
+				console.log("15");
+				break;
+			//tablet clear		
+			case"Derpbot clear tablet":
+			case"Hubot clear tablet":
+				msg.count = 21;
+				seeThrough(msg);
+				console.log("21");
+				break;
+			//God forbid you somehow do get past all the above text comparisons and didnt misscapitalize the bots will explain you need to learn to speak clearly.
+			default:
+				msg.send("Hey! I can't understand you when you mumble make sure to capitalize my awesome name, all right.")
 		}
 	});
 }
