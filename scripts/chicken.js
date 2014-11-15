340d0ee43839588eedf273b79ba177fc610668a4
Matthew Beyer <matthewbeyer@fullsail.edu>
2014-11-14 20:39:53 -0600

    chicken script Matthew Beyer

---
+++ scripts/chicken.js
@@ -0,0 +1,11 @@
+//Why did the chicken cross the road
+module.exports = (robot) ->
+
+robot.respond /chicken$/i, (msg) ->
+msg.send "Why did the chicken cross the road? To get to the other side hahaha"
+
+robot.respond /dog$/i, (msg) ->
+msg.send "ruff ruff"
+
+robot.respond /cat$/i, (msg) ->
+msg.send "meow meow"
