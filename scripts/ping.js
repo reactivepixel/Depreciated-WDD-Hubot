9dcbd261dd1a563c8414066a07e1d7ef40a7327a
Matthew Beyer <matthewbeyer@fullsail.edu>
2014-11-15 11:27:03 -0600

    changed comments structure

--- scripts/ping.js
+++ scripts/ping.js
@@ -1,11 +1,11 @@
-# Description:
-#   Utility commands surrounding Hubot uptime.
-#
-# Commands:
-#   hubot ping - Reply with pong
-#   hubot echo <text> - Reply back with <text>
-#   hubot time - Reply with current time
-#   hubot die - End hubot process
+// Description:
+//   Utility commands surrounding Hubot uptime.
+//
+// Commands:
+//   hubot ping - Reply with pong
+//   hubot echo <text> - Reply back with <text>
+//  hubot time - Reply with current time
+//   hubot die - End hubot process
 
 module.exports = function(robot) {
   robot.respond(/PING$/i, function(msg) {
