89ba2775c8a6bb1595a6e9566f807dc029966eac
Bobby Newland <bobbynewland@admins-MacBook-Pro.local>
2014-11-17 19:07:32 -0500

    testing changed sing back to pong

--- scripts/ping.js
+++ scripts/ping.js
@@ -9,7 +9,7 @@
 
 module.exports = function(robot) {
   robot.respond(/PING$/i, function(msg) {
-    return msg.send("sing");
+    return msg.send("PONG");
   });
   robot.respond(/ADAPTER$/i, function(msg) {
     return msg.send(robot.adapterName);
