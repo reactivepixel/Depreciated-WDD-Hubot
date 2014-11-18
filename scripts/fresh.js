23d8662660ec305abb17160dae53b8f2f8ed0433
Bobby Newland <bobbynewland@admins-MacBook-Pro.local>
2014-11-18 18:25:14 -0500

    added new script

--- scripts/fresh.js
+++ scripts/fresh.js
@@ -10,5 +10,9 @@ module.exports = function(robot) {
     return msg.send("http://img.pandawhale.com/43655-carlton-dancing-gif-8Bku.gif");
   });
 
+    return robot.hear(/^REALLY$/i, function(msg) {
+    return msg.send("http://i1242.photobucket.com/albums/gg534/AlwayzNToSumthin/Just%20Some%20Excess/tumblr_m70vbsTdrB1rropv0.gif?t=1342664144");
+  });	    
+
 
 };
