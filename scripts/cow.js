58f0937659ed94454e359f9e5f464710a3f1dda1
Matthew Beyer <matthewbeyer@fullsail.edu>
2014-11-20 07:10:36 -0600

    Added comment fixed indention

--- scripts/cow.js
+++ scripts/cow.js
@@ -15,7 +15,8 @@
 //matthew@beyerbuilds.com
 
 module.exports = function(robot) {
-robot.respond(/cow$/i, function(msg) {
-return msg.send("http://i.imgur.com/G2YpErL.jpg#.png"); // Holly Springsteen - use .jpg extension instead of page address (last extension must be png to display)
-});
+	return robot.respond(/cow$/i, function(msg) {
+		//hubot returns the image/image link
+		msg.send("http://i.imgur.com/G2YpErL.jpg#.png"); // Holly Springsteen - use .jpg extension instead of page address (last extension must be png to display)
+	});
 };
