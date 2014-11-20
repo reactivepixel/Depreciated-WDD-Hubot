fcf2f5231ed4e9c65130a047586dc910444bcf81
Matthew Beyer <matthewbeyer@fullsail.edu>
2014-11-20 07:42:01 -0600

    used jslint for animalsounds.js

--- scripts/animalsounds.js
+++ scripts/animalsounds.js
@@ -16,17 +16,17 @@
 //Mathew Beyer
 //matthew@beyerbuilds.com
 
-module.exports = function(robot) {
-	//returns coo coo after users inputs chicken
-	robot.respond(/chicken$/i, function(msg) {
-	return msg.send("coo coo");
-	});
-	//returns ruff ruff after users inputs dog
-	robot.respond(/dog$/i, function(msg) {
-	return msg.send("ruff ruff");
-	});
-	//returns meow meow after users inputs cat
-	robot.respond(/cat$/i, function(msg) {
-	return msg.send("meow meow");
-	});
+module.exports = function (robot) {
+    //returns coo coo after users inputs chicken
+    robot.respond(/chicken$/i, function (msg) {
+        return msg.send('coo coo');
+    });
+    //returns ruff ruff after users inputs dog
+    robot.respond(/dog$/i, function (msg) {
+        return msg.send('ruff ruff');
+    });
+    //returns meow meow after users inputs cat
+    robot.respond(/cat$/i, function (msg) {
+        return msg.send('meow meow');
+    });
 };
