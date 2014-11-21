e47022daedaa393b1e6321fbd081c8e9a79a1be9
Clayton Allen <Clayton@Claytons-MacBook-Pro-2.local>
2014-11-20 18:40:33 -0600

    Testing Red Solo Push
    This is just a test to see that a push was successful.

---
+++ scripts/redsolocup.js
@@ -0,0 +1,22 @@
+// Description:
+//   Hubot can now tell you how many red solo cups are needed to build pyramid
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   None
+//
+// Commands:
+//   Hubot red solo cup pyramid <height> - Enter height, get red solo cup count
+//
+// Author:
+//  Clayton Allen
+//  clayton.allen.us@gmail.com
+
+module.exports = function(robot){
+
+	return robot.respond('/red solo cup/i', function(msg){
+		msg.send('Oh so you want to build a Red Solo Cup Pyramid, just one second while I calculate that');
+	});
+}
