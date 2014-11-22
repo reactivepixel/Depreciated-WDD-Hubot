88a9b76113d3d0304c90f25a7171ef3dab67358d
Clayton Allen <Clayton@Claytons-MacBook-Pro-2.local>
2014-11-21 20:49:56 -0600

    Initial Taste of the rainbow script built, now time to watch the fireworks.

---
+++ scripts/tastetherainbow.js
@@ -0,0 +1,37 @@
+// Description:
+// Hubot Random Color Generator
+//
+// Dependencies:
+// request
+//
+// Configuration:
+// none
+//
+// Commands:
+// Hubot tastetherainbow - Gives you a cool new random color to use in your web or graphic design!
+//
+// Author:
+// Clayton Allen
+// 
+// Email:
+// clayton.allen.us@gmail.Commands
+
+var request = require('request');
+
+function tastetherainbow(msg){
+	request('http://www.colourlovers.com/api/colors/random?format=json', function (error, response, body){
+		if (!error && response.statusCode < 300){
+			var colorData = JSON.parse(response.body),
+				colorName = colorData[1];
+			msg.send("Your color name is " + colorName);
+		}else{
+			msg.send("Yuck!!! That rainbow was nasty... Try running that again");
+		}
+	});
+}
+
+module.exports = function(robot) {
+  return robot.respond(/tastetherainbow/i, function(msg) {
+ 		tastetherainbow(msg);
+  });
+}
