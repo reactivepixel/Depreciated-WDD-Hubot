ec0d559a23b863b6b9b95c45169db288c89998ca
Clayton Allen <Clayton@Claytons-MacBook-Pro-2.local>
2014-11-21 22:14:14 -0600

    Okay formatting the output

--- scripts/tastetherainbow.js
+++ scripts/tastetherainbow.js
@@ -16,26 +16,45 @@
 // Email:
 // clayton.allen.us@gmail.Commands
 
+// Dependencies
 var request = require('request');
 
+// Taste The Rainbow Functio
 function tastetherainbow(msg){
+
+	// Building the request to colourlovers
 	request('http://www.colourlovers.com/api/colors/random?format=json', function (error, response, body){
+
+		// If there are no errors from the response parse the JSON
 		if (!error && response.statusCode < 300){
 			var colorData = JSON.parse(response.body),
+
+				// Get the name of the Color
 				colorName = colorData[0].title;
+
+				// Grab the color image returned from the response
 				colorCard = colorData[0].imageUrl;
-				
-			msg.send("Your color name is " + colorName);
+			
+			// Display the color name 
+			msg.send("Your color name is: " + colorName.toUpperCase());
+
+			// Output the color image
 			msg.send(colorCard);
 
 		}else{
+
+			// If there was an error in the request rensponse tell the user something evil is lurking and they need to retry
 			msg.send("Yuck!!! That rainbow was nasty... Try running that again");
 		}
 	});
 }
 
 module.exports = function(robot) {
-  return robot.respond(/tastetherainbow/i, function(msg) {
+
+	// Listen for the command
+	return robot.respond(/tastetherainbow/i, function(msg) {
+
+		// Taste the rainbow homie!!!
  		tastetherainbow(msg);
   });
 }
