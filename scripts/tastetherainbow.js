29e45a0028f19273bb16cd2d647b3751e5d73f17
Clayton Allen <Clayton@Claytons-MacBook-Pro-2.local>
2014-11-21 21:19:49 -0600

    Attempt to grab JSON data 1

--- scripts/tastetherainbow.js
+++ scripts/tastetherainbow.js
@@ -21,7 +21,7 @@ var request = require('request');
 function tastetherainbow(msg){
 	request('http://www.colourlovers.com/api/colors/random?format=json', function (error, response, body){
 		if (!error && response.statusCode < 300){
-			var colorData = JSON.parse(response.body),
+			var colorData = JSON.parse(response),
 				colorArr = [];
 				for(var i = 0; i < colorData.length; i++){
 					colorArr.push(colorData[i]);
