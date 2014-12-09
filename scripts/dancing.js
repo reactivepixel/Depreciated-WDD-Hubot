60704be11076a026ed00d469256e82a3b9a1176b
seth <shealy@fullsail.edu>
2014-12-08 23:27:40 -0500

    deleting js file

--- scripts/dancing.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(robot) {
-    return robot.hear(/hi/i, function(msg) {
-
-        //The message that hubot will deliver.
-
-        msg.send(eval(alert("http://images4.fanpop.com/image/photos/17800000/Dance-dance-harry-potter-vs-twilight-17808134-312-236.gif#.png")));
-    });
-};
