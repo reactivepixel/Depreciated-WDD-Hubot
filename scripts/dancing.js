ec6bd1b7caa37aa855ef2471ccc76b009b459c85
seth <shealy@fullsail.edu>
2014-12-08 22:36:37 -0500

    testing dancing.js for reference to future assignment

---
+++ scripts/dancing.js
@@ -0,0 +1,8 @@
+module.exports = function(robot) {
+    return robot.hear(/hi/i, function(msg) {
+
+        //The message that hubot will deliver.
+
+        msg.send("http://images4.fanpop.com/image/photos/17800000/Dance-dance-harry-potter-vs-twilight-17808134-312-236.gif#.png");
+    });
+};
