1fccd84bea372930e7836433d93866676c99ddc4
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2014-12-08 22:24:17 -0500

    added enviroment id and key

--- scripts/whats-for-dinner.js
+++ scripts/whats-for-dinner.js
@@ -3,6 +3,8 @@
 //
 // Dependencies:
 //   var request = require('request');
+//   YUMMLY_API_KEY
+//   YUMMLY_APP_ID
 //
 // Configuration:
 //   None
@@ -16,18 +18,20 @@
 
 //Dependencie for url load.
 var request = require('request');
+var YUMMLY_API_KEY= process.env.YUMMLY_API_KEY
+var YUMMLY_APP_ID= process.env.YUMMLY_APP_ID
 
 
 function food(msg){
   var randomINT = Math.floor((Math.random() * 200000) + 1);//random int for recepie array.
-  var apiURL = 'http://api.yummly.com/v1/api/recipes?_app_id=2f555d00&_app_key=3a78da9d272c848b3d6dd9665b9a35f7&q=""&maxResult=1&start=' + randomINT; // API conection
+  var apiURL = 'http://api.yummly.com/v1/api/recipes?_app_id='+YUMMLY_APP_ID+'&_app_key='+YUMMLY_APP_ID+'&q=""&maxResult=1&start=' + randomINT; // API conection
   
   request(apiURL, function (error, response, body){
     if (!error && response.statusCode < 300){
       var food = JSON.parse(body); //JSON parsing for the food object.
       msg.send(food.matches[0].recipeName); //This is the return for the food function.
       
-      var readById = "http://api.yummly.com/v1/api/recipe/"+food.matches[0].id+"?_app_id=2f555d00&_app_key=3a78da9d272c848b3d6dd9665b9a35f7"; //API url for additional information.
+      var readById = "http://api.yummly.com/v1/api/recipe/"+food.matches[0].id+"?_app_id="+YUMMLY_APP_ID+"&_app_key="+YUMMLY_APP_ID; //API url for additional information.
       
       request(readById, function (error, response, body){
         var recipesID = JSON.parse(body);// this reads the additionl content for the recepie
