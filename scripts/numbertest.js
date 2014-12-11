5c5e5751b5e4b2490fb432d8b180f734f1f7bc4c
Jordan Wilson <MrWizard@jordans-mbp.fullsail.com>
2014-12-10 23:39:22 -0500

    Game now resets. working on ending the game

--- scripts/numbertest.js
+++ scripts/numbertest.js
@@ -32,30 +32,51 @@ function numgame(msg){
 module.exports = function(robot) {
   robot.respond(/test/i, function(msg) {
  	 numgame(msg);
+ 	 setupObj.startGame();
   });	
   
-  	var tries = 3;
-    var magNum = Math.ceil(Math.random() * 10 - 1);
-    console.log("RANDOM NUM ", magNum);
+  robot.respond(/stop test/i, function(msg) {
+ 	 setupObj=null;
+  });
   
+ //  var setupObj = {
+// 				tries : 3,
+//     			magNum : Math.ceil(Math.random() * 10 - 1)
+//   };
+
+	var setupObj = false;
+  
+   setupObj.startGame = function(){
+		var tries = this.tries;
+		var magNum = this.magNum;
+    	console.log("###########RANDOM NUM!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ", magNum);
+   };
+  	
   robot.hear(/^\d{1,2}$/i, function(msg){
   		var guess = msg.match[0];
   		console.log("my guess ",guess);
   		
-    	if(guess < magNum){
-            msg.send("Sorry, your guess is too low. You have " + tries +" chances left. MAGIC:" +magNum);
-            tries--;
+    	if(guess < setupObj.magNum){
+            msg.send("Sorry, your guess is too low. You have " + setupObj.tries +" chances left. MAGIC:" +setupObj.magNum);
+            setupObj.tries--;
         }
-        else if(guess > magNum){
-            msg.send("Sorry, your guess is too high. You have " + tries + "chances left. MAGIC:" +magNum);
-            tries--;
+        else if(guess > setupObj.magNum){
+            msg.send("Sorry, your guess is too high. You have " + setupObj.tries + " chances left. MAGIC:" +setupObj.magNum);
+            setupObj.tries--;
         }
-        else if(guess == magNum){
-            msg.send("You got it!!! MAGIC:" +magNum);
+        else if(guess == setupObj.magNum){
+            msg.send("You got it!!! MAGIC:" +setupObj.magNum);
+            // setupObj.magNum = Math.ceil(Math.random() * 10 - 1);
+//             setupObj.tries = 3;
+//             setupObj.startGame();
+//             msg.send("We started a new game MAGIC:" +setupObj.magNum);
         }
-        if(tries == 0){
-            msg.send("You have no more chances left! MAGIC:" +magNum);
-            
+        if(setupObj.tries == 0){
+            msg.send("You have no more chances left! MAGIC:" +setupObj.magNum);
+            setupObj.magNum = Math.ceil(Math.random() * 10 - 1);
+            setupObj.tries = 3;
+            setupObj.startGame();
+            msg.send("We started a new game MAGIC:" +setupObj.magNum);
         }
     });
 }
