9bf28e62ec5b52850d9e16cbc69c69b9c88c9a17
Jordan Wilson <MrWizard@jordans-mbp.fullsail.com>
2014-12-15 20:23:31 -0500

    added time delay functions to duel.js

--- scripts/MagicNums.js
+++ scripts/MagicNums.js
@@ -18,9 +18,9 @@
 function numGame(msg){
 
 	msg.send("Let's play Magic Numbers! :D");// Intro.
-    msg.send("Pick a number between 0-10 and I'll tell you if you're high, low or spot on."); 
-    msg.send("You have 3 tries.");
-    msg.send("What's your guess?");
+	msg.send("Pick a number between 0-10 and I'll tell you if you're high, low or spot on."); 
+	msg.send("You have 3 tries.");
+	msg.send("What's your guess?");
 
 };
 
@@ -33,39 +33,39 @@ module.exports = function(robot) {
   
 robot.respond(/quit$/i, function(msg) {
 	setupObj.playing=false;
- 	msg.send("Oh really? What if I wanted to play again? No? Go quit then.");//Quit the game.
+	msg.send("Oh really? What if I wanted to play again? No? Go quit then.");//Quit the game.
   });
 
 	var setupObj = {};//This object holds the rules of the game.
   	//The rules.
-   setupObj.startGame = function(){
-		this.tries = 3;
+  	setupObj.startGame = function(){
+   		this.tries = 3;
 		this.magNum = Math.ceil(Math.random() * 10 - 1);
 		this.playing = true;
-   };
+};
   	
-  robot.hear(/^\d{1,2}$/i, function(msg){
-  		var guess = msg.match[0];//Looks for a one or two digit number.
+robot.hear(/^\d{1,2}$/i, function(msg){
+	var guess = msg.match[0];//Looks for a one or two digit number.
   		
-    	if(guess < setupObj.magNum && setupObj.playing){
-    		setupObj.tries--;//Guess is too low.
-            msg.send("Sorry, your guess is too low. You have " + setupObj.tries +" chances left.");
-        }
-        else if(guess > setupObj.magNum && setupObj.playing){
-        	setupObj.tries--;//Guess is too high.
-            msg.send("Sorry, your guess is too high. You have " + setupObj.tries + " chances left.");
-        }
-        else if(guess == setupObj.magNum && setupObj.playing){
-            msg.send("You got it!!!!!!!!!!!!!!!");//Winner winner chicken dinner!
-            setupObj.startGame();
-            msg.send("Nice going! Let's play again! Or would you like to 'quit'?");
-            msg.send("If you want to play, what's your guess?");
-        }
-        if(setupObj.tries == 0 && setupObj.playing){
-            msg.send("You have no more chances left!");//Lost the game.
-            setupObj.startGame();
-            msg.send("Tough luck! Would you like to try again, Or would you like to 'quit'?");
-            msg.send("If you want to play, what's your guess?");
+    if(guess < setupObj.magNum && setupObj.playing){
+    	setupObj.tries--;//Guess is too low.
+        msg.send("Sorry, your guess is too low. You have " + setupObj.tries +" chances left.");
+    }
+    else if(guess > setupObj.magNum && setupObj.playing){
+		setupObj.tries--;//Guess is too high.
+		msg.send("Sorry, your guess is too high. You have " + setupObj.tries + " chances left.");
+    }
+    else if(guess == setupObj.magNum && setupObj.playing){
+    	msg.send("You got it!!!!!!!!!!!!!!!");//Winner winner chicken dinner!
+        setupObj.startGame();
+        msg.send("Nice going! Let's play again! Or would you like to 'quit'?");
+        msg.send("If you want to play, what's your guess?");
+    }
+    if(setupObj.tries == 0 && setupObj.playing){
+        msg.send("You have no more chances left!");//Lost the game.
+        setupObj.startGame();
+        msg.send("Tough luck! Would you like to try again, Or would you like to 'quit'?");
+        msg.send("If you want to play, what's your guess?");
         }
     });
 }
--- scripts/beer.js
+++ scripts/beer.js
@@ -17,21 +17,21 @@ var request = require('request');
 var key = process.env.BREWERYDB_KEY;
 function beerRun(msg){
 
-    // a variable to hold the api url
-    var apiURL = 'http://api.brewerydb.com/v2/beer/random?key=' + key;
-    // API request
-    request(apiURL, function (error, response, body) {
-        if (!error && response.statusCode < 300){
-                //parse the json
-                var json = JSON.parse(response.body);
-                // finds a random beer from the api and brings back the name, style and description 
-                var beer = "Beer Name: "+json.data.name +"\n\n"+"Style: "+json.data.style.category.name+"\n\n"+"Description: "+json.data.style.description;
+	// a variable to hold the api url
+	var apiURL = 'http://api.brewerydb.com/v2/beer/random?key=' + key;
+	// API request
+	request(apiURL, function (error, response, body) {
+	if (!error && response.statusCode < 300){
+    	//parse the json
+    	var json = JSON.parse(response.body);
+        // finds a random beer from the api and brings back the name, style and description 
+        var beer = "Beer Name: "+json.data.name +"\n\n"+"Style: "+json.data.style.category.name+"\n\n"+"Description: "+json.data.style.description;
                 
-                msg.send(beer);//displays the random beer 
+        	msg.send(beer);//displays the random beer 
                 
         }else{
             //sends an error message if something goes wrong
-            msg.send("We are having some technical difficulties server side...");
+        	msg.send("We are having some technical difficulties server side...");
         } 
     });
 
