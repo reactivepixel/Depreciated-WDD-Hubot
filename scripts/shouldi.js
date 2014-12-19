18bcbd1e0049e02b4f76b0db091682f6ffc17fc0
Daniel Carroll <DCarroll@fullsail.edu>
2014-12-18 21:35:16 -0600

    Fixed redundant response issue

--- scripts/shouldi.js
+++ scripts/shouldi.js
@@ -19,11 +19,19 @@ Contributor:
 module.exports = function(robot) {
     robot.respond(/should I (.*)$/i, function(msg) {
         msg.send("Is your name Daniel?");
-        robot.hear(/yes$/i, function(msg){
-            msg.send("Then you already know what to do.");
-        });
-        robot.hear(/no$/i, function(msg){
-            msg.send("You should ask Daniel.")
-        })
+        responseNumber = 0;
+            robot.hear(/yes$/i, function (msg) {
+                if(responseNumber === 0) {
+                    msg.send("Then you already know what to do.");
+                    responseNumber = responseNumber + 1;
+                }
+            });
+            robot.hear(/no$/i, function (msg) {
+                if(responseNumber === 0) {
+                    msg.send("You should ask Daniel.");
+                    responseNumber = responseNumber + 1;
+                }
+            })
+
     });
 };
