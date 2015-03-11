a1b1acd0a39ece9920649cba37cc6397124ad138
Drew Kroft <drewk2525@yahoo.com>
2015-03-11 04:10:01 -0400

    added random wiki article

---
+++ scripts/wiki.js
@@ -0,0 +1,38 @@
+// Description:
+// Random wiki article
+//
+// Dependencies:
+// none
+//
+// Configuration:
+// none
+//
+// Commands:
+// hubot wiki - Returns a random wiki article
+//
+// Author:
+// Andrew Kroft
+
+var request = require('request');
+
+// Function to find and return a random wiki article
+function getWiki(msg) {
+
+    request('http://en.wikipedia.org/w/api.php?action=query&list=random&rnlimit=1&format=json', function (error, response){
+        var json = JSON.parse(response.body);
+        //var json = msg.random(json.data.memes);
+        //msg.send("Meme: " + json.name);
+        wikiTitle = json.query.random[0].title;
+        var wikiURL = wikiTitle.replace(" ", "_");
+        wikiURL = "https://en.wikipedia.org/wiki/" + wikiURL;
+        msg.send("Wiki Title: " + wikiTitle);
+        msg.send("URL: " + wikiURL);
+    });
+
+} // End of "getMeme" function.
+
+module.exports = function(robot) {
+	return robot.respond(/wiki(.*)/i, function(msg) {
+		getWiki(msg);
+	});
+}
