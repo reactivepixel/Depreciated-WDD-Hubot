8dfdac3fc99a1a1a43fa527d1e6e5da9b874c28e
Drew Kroft <drewk2525@yahoo.com>
2015-03-11 11:54:17 -0400

    updated messages

--- scripts/wiki.js
+++ scripts/wiki.js
@@ -20,13 +20,11 @@ function getWiki(msg) {
 
     request('http://en.wikipedia.org/w/api.php?action=query&list=random&rnlimit=1&format=json', function (error, response){
         var json = JSON.parse(response.body);
-        //var json = msg.random(json.data.memes);
-        //msg.send("Meme: " + json.name);
         var wikiTitle = json.query.random[0].title;
         var wikiURL = wikiTitle.replace(/ /g, "_");
         wikiURL = "https://en.wikipedia.org/wiki/" + wikiURL;
         msg.send("Wiki Title: " + wikiTitle);
-        msg.send("URL: " + wikiURL);
+        msg.send("Wiki URL: " + wikiURL);
     });
 
 } // End of "getMeme" function.
