a588c3d985cebe297179dce6243460547597f84f
jjhales <jjhales@fullsail.edu>
2015-03-27 14:52:06 -0400

    added notes to dance.js

--- scripts/dance.js
+++ scripts/dance.js
@@ -13,11 +13,13 @@
 // Author: 
 //	 Juanita J Hales
 // 
+// Date:
+//    March 27, 2015
 
-
+//command robot dance
 module.exports = function (robot) {
     return robot.respond(/dance$/i, function (msg) {
-        //hubot returns Carlton
+        //hubot returns Carlton gif
         msg.send('http://i.imgur.com/gjsxU0s.gif');
     });
 };
