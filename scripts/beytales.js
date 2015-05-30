f445d3fdc375870abc975633937d05111fb4363c
SWilliamsFSU <subsbsc@fullsail.edu>
2015-05-30 19:18:23 -0400

    inital push to staging

---
+++ scripts/beytales.js
@@ -0,0 +1,21 @@
+// Description:
+//   Hubot Beytales. 
+//
+//
+// Commands:
+//	 Hubot beytales - hubot will show a video of Beyonce dancing to Ducktales
+//
+// Author: 
+//	 Steven Williams
+// 
+// Date:
+//    May 29, 2015
+
+
+module.exports = function (robot) {
+    return robot.respond(/beytales$/i, function (msg) {
+        //hubot displays video
+        msg.send('http://www.youtube.com/watch?v=-EW9yS1YhTE');
+    });
+};
+
---
+++ scripts/familyguy.js
@@ -0,0 +1,24 @@
+// Description:
+//   Hubot Family Guy. 
+//
+//
+// Commands:
+//	 Hubot what time is it - hubot will show you what time it is
+//
+// Author: 
+//	 Steven Williams
+// 
+// Date:
+//    May 28, 2015
+
+
+module.exports = function (robot) {
+    return robot.respond(/what time is it/i, function (msg) {
+        //hubot displays message
+        msg.send("It's Peanut Butter Jelly Time!");
+        //hubot displays gif
+        msg.send('http://studybreaks.com/sites/default/files/peanut%20butter.gif');
+        
+    });
+};
+
