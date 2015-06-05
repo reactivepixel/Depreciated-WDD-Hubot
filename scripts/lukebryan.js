f8e092dcb6853bb2069864e936f5b83b3e846f94
Stacy Faude <istacy@fullsail.edu>
2015-06-04 21:34:36 -0500

    Testing Luke Bryan - changed youTube URL

--- scripts/lukebryan.js
+++ scripts/lukebryan.js
@@ -15,11 +15,11 @@
 // istacy@fullsail.edu
 
 // array of Luke Bryan songs on youtube
-var songs = ["'Play it again' (https://www.youtube.com/watch?v=ALV-QtDFpSw)"];
+var songs = ["https://www.youtube.com/watch?v=ALV-QtDFpSw"];
 
 module.exports = function(robot) {
 	return robot.respond(/play luke bryan/i, function(msg) {
 		// hubot decide on a random song from the songs array
-		msg.send("I recommend " + msg.random(songs));
+		msg.send(msg.random(songs));
 	});
 };
