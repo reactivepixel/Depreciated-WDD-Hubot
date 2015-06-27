c91cbd9aec9ea6668f79edb4b774a5856282cd15
Shye <shye@fullsail.edu>
2015-06-26 20:55:24 -0400

    trending file

---
+++ scripts/trending.js
@@ -0,0 +1,20 @@
+// Description:
+//   Utility commands surrounding Hubot uptime.
+//
+// Hubot returns what's trending on twitter
+//
+// Commands
+//   hubot what's trending
+//
+//  Dependencies
+//      none
+//
+// author: Shye TG
+// shye@fuillsail.edu
+
+
+module.exports = function(robot) {
+    robot.hear(/trending$/i, function(msg) {
+        return msg.send('https://twitter.com/whatstrending');
+    });
+};
