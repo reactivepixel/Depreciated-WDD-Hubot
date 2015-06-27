36014cd5cf53c16a380ca589a606ba0b9d900640
Shye <shye@fullsail.edu>
2015-06-27 00:02:18 -0400

    edited trending.js

--- scripts/trending.js
+++ scripts/trending.js
@@ -20,7 +20,7 @@
 //Starts up the hubot interface
 module.exports = function(robot) {
     return robot.respond(/whats trending$/i, function(msg) {
-        //Picks a random trending site
-        msg.send(msg.random(trend) + "Check it out!");
+        
+        msg.send(msg.random(trend) + " Check it out!");
     });
 };
