92ab44b7eb6a0aa1fc9f366c3974728f48038cc2
Shye <shye@fullsail.edu>
2015-06-26 23:52:49 -0400

    trending url

--- scripts/trending.js
+++ scripts/trending.js
@@ -13,14 +13,14 @@
 // shye@fuillsail.edu
 
   var trend = [
+    "http://https://twitter.com/hashtag/trending",
     "http://whatstrending.com/categories/trending-now",
     "https://www.google.com/trends/hottrends",
-    "http://https://twitter.com/hashtag/trending",
     ];
 //Starts up the hubot interface
 module.exports = function(robot) {
     return robot.respond(/whats trending$/i, function(msg) {
         //Picks a random trending site
-        msg.send(msg.random(trend) + " Trending Right Now");
+        msg.send(msg.random(trend) + "Check it out!");
     });
 };
