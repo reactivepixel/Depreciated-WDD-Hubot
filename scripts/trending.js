d948f8fb0fad75a82adc29894811528e887aeba1
Shye <shye@fullsail.edu>
2015-06-26 23:30:49 -0400

    whats trending

--- scripts/trending.js
+++ scripts/trending.js
@@ -12,26 +12,15 @@
 // author: Shye TG
 // shye@fuillsail.edu
 
-  robot.http("http://whatstrending.com/categories/trending-now")
-    .header('Accept', 'application/json')
-    .get() (err, res, body) ->
-      // err & response status checking code here
-
-      if response.getHeader('Content-Type') isnt 'application/json'
-        res.send "Didn't get back JSON :("
-        return
-
-      data = null
-      try
-        data = JSON.parse body
-      catch error
-       res.send "Ran into an error parsing JSON :("
-       return
-
-      
-//module.exports = function(robot) {
-//    robot.hear(/trending$/i, function(msg) {
-//        return msg.send('https://twitter.com/whatstrending');
-//    });
-//};
-
+  var trend = [
+    "http://whatstrending.com/categories/trending-now",
+    "https://www.google.com/trends/hottrends",
+    "http://https://twitter.com/hashtag/trending",
+    ];
+//Starts up the hubot interface
+module.exports = function(robot) {
+    return robot.respond(/whats trending$/i, function(msg) {
+        //Picks a random trending site
+        msg.send(msg.random(trend) + " Trending Right Now");
+    });
+};
