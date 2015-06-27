ec0380ae888a3a7998b7c5d85baa4835baefa7c9
Rukiri <root@Ericks-MBP.midco.net>
2015-06-26 19:26:09 -0500

    updated button

--- scripts/muffin.js
+++ scripts/muffin.js
@@ -10,7 +10,12 @@
 // Lets press that button!
 module.exports = function(robot) {
 	// Type muffin button to get a muffin
-	robot.hear(/muffin_button$/i, function(msg) {
+	robot.hear(/muffin button/i, function(msg) {
     	return msg.send("Did you say muffin button?!" + " " + "https://www.youtube.com/watch?v=xpIeqU0OiAU");
+  	});
+
+  	// Respond to the muffin button
+  	robot.respond(/wheres that button that makes blueberry muffins/i, function(msg) {
+  		return msg.send("Did you say muffin button?!" + " " + "https://www.youtube.com/watch?v=xpIeqU0OiAU");
   	});
 };
