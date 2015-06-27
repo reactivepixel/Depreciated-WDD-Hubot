4a373e2eca4e56c809717c56ac52877c908f66fa
Shye <shye@fullsail.edu>
2015-06-26 22:13:41 -0400

    update trending

--- scripts/trending.js
+++ scripts/trending.js
@@ -12,10 +12,24 @@
 // author: Shye TG
 // shye@fuillsail.edu
 
+var request = require('request');
 
-module.exports = function(robot) {
-    robot.hear(/trending$/i, function(msg) {
-        return msg.send('https://twitter.com/whatstrending');
-    });
-};
+module.exports = function (robot) {
+    
+    //the URL to twitter
+    var URL = 'https://twitter.com/whatstrending';
+    
+    //whats trending?
+    robot.respond(/whats trending?/i, function (msg) {
+        
+        //Make hubot respond with URL
+        msg.send("URL");
+        
+       
+
+//module.exports = function(robot) {
+//    robot.hear(/trending$/i, function(msg) {
+//        return msg.send('https://twitter.com/whatstrending');
+//    });
+//};
 
