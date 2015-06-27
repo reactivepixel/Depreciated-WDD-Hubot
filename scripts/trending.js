07c53dc3d11426339879da6f2b8425c8c5c1a0b1
Shye <shye@fullsail.edu>
2015-06-27 00:22:09 -0400

    link updates

--- scripts/trending.js
+++ scripts/trending.js
@@ -13,11 +13,9 @@
 // shye@fuillsail.edu
 
   var trend = [
-    "<a class="twitter-timeline" href="https://twitter.com/hashtag/trending" data-widget-id="614646419614404608">#trending Tweets</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>""
-    //"http://twitter.com/hashtag/trending",
-    //"http://whatstrending.com/categories/trending-now",
-    //"http://www.buzzfeed.com/trending",
+   "https://twitter.com/hashtag/trending",
+   "http://whatstrending.com/categories/trending-now",
+   "http://www.buzzfeed.com/trending",
     ];
 //Starts up the hubot interface
 module.exports = function(robot) {
