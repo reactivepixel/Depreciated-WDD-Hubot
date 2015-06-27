136fb2f595bd0ca3138b42269794b97071ca7cb3
bryan0578 <bryan.cash78@gmail.com>
2015-06-27 17:29:24 -0400

    finished 3 scripts darthVader.js, AT-AT.js, and starWars7.js

--- scripts/starWars7.js
+++ scripts/starWars7.js
@@ -21,20 +21,26 @@
     module.exports = function(robot) {
         return robot.respond(/Star Wars 7/i, function(msg) {
             //set the release date for Star Wars The Force Awakens
-            now = new Date();
-            eventDate = new Date("December 17, 2001");
+            currentDate = new Date();
+            //set the release date for Star Wars The Force Awakens
+            releaseDate = new Date(2015,11,18);
+            //calculate the milliseconds for each day
             msEachDay = 24 * 60 * 60 * 1000;
-            console.log (msEachDay);
+            
+            //calculate the days remaining until the release date
+            daysRemaining = (eventDate - now)/msEachDay;
+                    
+            //round the number of days remaining
+            daysRemaining = Math.round(daysRemaining);
 
-            function daysUntilEvent() {
-                // Step 1:
-                daysRemaining = (eventDate.getTime() - now.getTime()) /
-                    msEachDay;
-                    console.log(daysRemaining);
-                // Step 2:
-                daysRemaining = Math.round(daysRemaining);
-                // Step 3:
-                msg.send("There is approximately " +
+            //if the calculation result is less than 0 it has passed the release date
+            if(daysRemaining<0){
+            	//hubot respods with the movie is out now
+            	msg.send("Star Wars The Force Awakens is out now!")
+            }else{
+            	//if the calculation result is greater than zero
+            	//hubot respods with the number of days until the release
+            	msg.send("There is approximately " +
                     daysRemaining + " days until Star Wars The Force Awaknes arrives in theaters!");
             }
         });
